07e6b7b92b7bf68f72d66f5e324933230144113d
Jacob Lewallen <jlewalle@gmail.com>
2017-09-02 07:42:44 -0700

    Errors, retry responses.

---
+++ fk-module-protocol.h
@@ -0,0 +1,11 @@
+#ifndef FK_MODULE_PROTOCOL_INCLUDED
+#define FK_MODULE_PROTOCOL_INCLUDED
+
+#define FK_MODULE_PROTOCOL_VERSION          0x1
+#define FK_MODULE_PROTOCOL_MAX_MESSAGE      128
+
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include "fk-module.pb.h"
+
+#endif
