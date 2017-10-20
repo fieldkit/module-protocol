dfab75f53e60b799b47f7be91ef4294cb08849f3
Jacob Lewallen <jlewalle@gmail.com>
2017-10-19 20:39:35 -0700

    Break sensor caps querying into separate queries.
    Going to try and keep this protocol small for the sake of I2C buffers. We'll see
    how much this complicates things.

--- src/fk-module-protocol.h
+++ src/fk-module-protocol.h
@@ -2,10 +2,17 @@
 #define FK_MODULE_PROTOCOL_INCLUDED
 
 #define FK_MODULE_PROTOCOL_VERSION          0x1
-#define FK_MODULE_PROTOCOL_MAX_MESSAGE      128
+#define FK_MODULE_PROTOCOL_MAX_MESSAGE      64
 
 #include <pb_encode.h>
 #include <pb_decode.h>
 #include "fk-module.pb.h"
+
+#ifdef ARDUINO_SAMD_ZERO
+#include <RingBuffer.h>
+#if FK_MODULE_PROTOCOL_MAX_MESSAGE > SERIAL_BUFFER_SIZE
+#error Module protocol too large for I2C buffer.
+#endif
+#endif
 
 #endif
